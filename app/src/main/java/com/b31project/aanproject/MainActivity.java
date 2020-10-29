1a3eaaa4cef9a945aada07650c3d83794afbe708
Faris Ahmed T. Alotaibi <faa339@nyu.edu>
2020-10-29 00:57:17 -0400

    Search working (mostly), Nav in progress

--- app/src/main/java/com/b31project/aanproject/MainActivity.java
+++ app/src/main/java/com/b31project/aanproject/MainActivity.java
@@ -1,15 +1,27 @@
 package com.b31project.aanproject;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
 import com.mapbox.mapboxsdk.location.modes.CameraMode;
@@ -18,6 +30,14 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+
 
 import java.util.List;
 
@@ -27,9 +47,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private MapView mapView;
     private MapboxMap mapboxMap;
     private PermissionsManager permManage;
+    private String geojsonSourceLayerId = "geojsonSourceLayerId";
+    private String symbolIconId = "symbolIconId";
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Load the mapview, create evrything needed
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_main);
@@ -39,18 +63,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     public void onMapReady(@NonNull final MapboxMap mapboxMap){
+        //Display the map with the appropriate style -- found on the mapbox styles site
         MainActivity.this.mapboxMap = mapboxMap;
-        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/aanprojectteam/ckg0ewsj31vsk19o83pshzome"),
+        //new Style.Builder().fromUri("mapbox://styles/aanprojectteam/ckg0ewsj31vsk19o83pshzome"
+        mapboxMap.setStyle(Style.MAPBOX_STREETS,
                 new Style.OnStyleLoaded(){
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
                         enableLocationComponent(style);
+                        initSearchBtn();
+                        style.addSource(new GeoJsonSource(geojsonSourceLayerId));
+                        style.addLayer(new SymbolLayer("SYMBOL_LAYER_ID", geojsonSourceLayerId)
+                                .withProperties(iconImage(symbolIconId), iconOffset(new Float[] {0f, -8f})));
+
                     }
                 });
     }
 
+    private void initSearchBtn(){
+        findViewById(R.id.fab_location_search).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new PlaceAutocomplete.IntentBuilder()
+                        .accessToken(getString(R.string.mapbox_access_token))
+                        .placeOptions(PlaceOptions.builder()
+                                      .backgroundColor((Color.parseColor("#EEEEEE")))
+                                      .limit(10)
+                                      .build(PlaceOptions.MODE_CARDS))
+                        .build(MainActivity.this);
+                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+            }
+        });
+    }
+
     @SuppressWarnings({"MissingPermission"})
     private void enableLocationComponent(@NonNull Style loadedMapStyle){
+        //Enable user location if it can find it already enables, else ask to use permissions
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
             LocationComponent locationComponent = mapboxMap.getLocationComponent();
             locationComponent.activateLocationComponent(LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
@@ -91,6 +139,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE){
+            CarmenFeature selectedCarFeat = PlaceAutocomplete.getPlace(data);
+            if (mapboxMap!=null){
+                Style style = mapboxMap.getStyle();
+                if (style!=null){
+                    GeoJsonSource source = style.getSourceAs(geojsonSourceLayerId);
+                    if (source!=null){
+                        source.setGeoJson(FeatureCollection.fromFeatures(new Feature[]
+                                {Feature.fromJson(selectedCarFeat.toJson())}));
+                    }
+                }
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+                        new CameraPosition.Builder()
+                                .target(new LatLng(((Point) selectedCarFeat.geometry()).latitude(),
+                                        ((Point) selectedCarFeat.geometry()).longitude()))
+                                .zoom(14)
+                                .build()), 4000);
+            }
+        }
     }
 
     @Override
